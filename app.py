ed4993c2331322e867e1c8df968387e006724003
Dieine Harumi Tacao <dtacaoharumi@gmail.com>
2025-07-24 13:23:27 -0500

    Add comentario

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 # ✅ Path for log file
 LOG_FILE = "chat_logs.txt"
-
+#test
 def save_chat_log(user_message, bot_response):
     """
     Save the conversation to a text file (chat_logs.txt)
